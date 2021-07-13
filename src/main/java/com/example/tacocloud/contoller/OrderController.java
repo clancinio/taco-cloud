747b4cf88a6540b7a156c95b2742edbafceba683
Clancinio <dean.clancy91@gmail.com>
2021-07-13 10:46:58 +0100

    Validating a POSTed Order

--- src/main/java/com/example/tacocloud/contoller/OrderController.java
+++ src/main/java/com/example/tacocloud/contoller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.tacocloud.contoller;
 import com.example.tacocloud.dto.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(Order order, Errors errors) {
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
+
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
