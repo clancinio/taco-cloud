ec2197045ec5931dd9ef26ce42ef7aeac13e65df
Clancinio <dean.clancy91@gmail.com>
2021-07-13 10:43:32 +0100

    Validate a POSTED Taco

--- src/main/java/com/example/tacocloud/contoller/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/contoller/DesignTacoController.java
@@ -5,9 +5,12 @@ import com.example.tacocloud.dto.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,7 +48,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco taco) {
+    public String processDesign(@Valid Taco taco, Errors errors) {
+
+        if(errors.hasErrors()){
+            return "design";
+        }
         // Save the taco design...
         // We'll do this in chapter 3
         log.info("Processing design: " + taco);
